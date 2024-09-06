dc375b2090c507f35dc92b531ae6c1a6c5940d9e
Teerapat Kirdpaiboon <teerapat.kir@dome.tu.ac.th>
2024-09-06 16:06:28 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/valuepassing3/MainActivity.java
+++ app/src/main/java/com/example/valuepassing3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.valuepassing3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        final Button submit_btn = findViewById(R.id.subnit_btn);
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+                EditText edt = findViewById(R.id.edit_text);
+                String value = edt.getText().toString();
+                intent.putExtra("param", value);
+                startActivity(intent);
+            }
         });
     }
 }
